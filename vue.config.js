cf29992ab777cdf14d7df131ff246719da4304d4
SurendarAadhirai <suren.soundarrajan@gmail.com>
2025-07-02 16:47:19 +0530

    auto deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'https://github.com/SurendarAadhirai/dot-matrix'
+  publicPath: '/dot-matrix/'
 })
